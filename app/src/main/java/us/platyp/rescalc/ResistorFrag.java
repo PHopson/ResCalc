076fa25bf14810c8984e1a49467abf3bbddbbc5c
Pete Hopson <peter.c.hopson@gmail.com>
2014-07-05 15:28:18 -0400

    Basic functionality done

--- app/src/main/java/us/platyp/rescalc/ResistorFrag.java
+++ app/src/main/java/us/platyp/rescalc/ResistorFrag.java
@@ -16,6 +16,7 @@ public class ResistorFrag extends Fragment implements AdapterView.OnItemSelected
 	int numBands;
 	TextView finalView, subtotalView;
 	Spinner sig_one, sig_two, sig_three, multiplier, tolerance, temperature;
+    View sig1_color, sig2_color, sig3_color, multi_color, tol_color, temp_color;
 
     public ResistorFrag() {
         // Required empty public constructor
@@ -60,18 +61,25 @@ public class ResistorFrag extends Fragment implements AdapterView.OnItemSelected
         
         //Set up significant digit spinners
         sig_one = (Spinner) getActivity().findViewById(R.id.sig1_band);
-        sig_one.setAdapter(allColorsAdapter); 
+        sig_one.setAdapter(allColorsAdapter);
+        //sig_one.setSelection(0,false);
         sig_one.setOnItemSelectedListener(this);
 
+        sig1_color = getActivity().findViewById(R.id.sig1_color);
+
         sig_two = (Spinner) getActivity().findViewById(R.id.sig2_band);
-        sig_two.setAdapter(allColorsAdapter); 
+        sig_two.setAdapter(allColorsAdapter);
         sig_two.setOnItemSelectedListener(this);
-        
+
+        sig2_color = getActivity().findViewById(R.id.sig2_color);
+
         //Five and six bands have an extra significant digit
         if (numBands == 5 || numBands == 6) {
             sig_three = (Spinner) getActivity().findViewById(R.id.sig3_band);
             sig_three.setAdapter(allColorsAdapter);
             sig_three.setOnItemSelectedListener(this);
+
+            sig3_color = getActivity().findViewById(R.id.sig3_color);
         }
         
 
@@ -82,6 +90,8 @@ public class ResistorFrag extends Fragment implements AdapterView.OnItemSelected
         multiplier = (Spinner) getActivity().findViewById(R.id.multiplier_band);
         multiplier.setAdapter(multiAdapter);
         multiplier.setOnItemSelectedListener(this);
+
+        multi_color = getActivity().findViewById(R.id.multiplier_color);
         
         //Tolerance spinner
         AllColorAdapter tolAdapter = 
@@ -90,6 +100,8 @@ public class ResistorFrag extends Fragment implements AdapterView.OnItemSelected
         tolerance = (Spinner) getActivity().findViewById(R.id.tolerance_band);
         tolerance.setAdapter(tolAdapter);
         tolerance.setOnItemSelectedListener(this);
+
+        tol_color = getActivity().findViewById(R.id.tolerance_color);
         
         //Temperature spinner for six bands
         if (numBands == 6) {
@@ -99,6 +111,8 @@ public class ResistorFrag extends Fragment implements AdapterView.OnItemSelected
             temperature = (Spinner) getActivity().findViewById(R.id.tempco_band);
             temperature.setAdapter(tempAdapter);
             temperature.setOnItemSelectedListener(this);
+
+            temp_color = getActivity().findViewById(R.id.temperature_color);
         }
         
         //Initialize text views
@@ -140,14 +154,33 @@ public class ResistorFrag extends Fragment implements AdapterView.OnItemSelected
     }
     
     public void UpdateColors() {
-    	View sig1_block, sig2_block, multi_block, tol_block;
-    	
+
+    	int[] baseColors = getResources().getIntArray(R.array.base_color_vals);
+        int[] multiColors = getResources().getIntArray(R.array.multi_color_vals);
+        int[] tolColors = getResources().getIntArray(R.array.tol_color_vals);
+
+        sig1_color.setBackgroundColor(baseColors[sig_one.getSelectedItemPosition()]);
+        sig2_color.setBackgroundColor(baseColors[sig_two.getSelectedItemPosition()]);
+
+        multi_color.setBackgroundColor(multiColors[multiplier.getSelectedItemPosition()]);
+
+        tol_color.setBackgroundColor(tolColors[tolerance.getSelectedItemPosition()]);
+
+        if (numBands == 5 || numBands == 6) {
+            sig3_color.setBackgroundColor(baseColors[sig_three.getSelectedItemPosition()]);
+        }
+
+        if (numBands == 6) {
+            int[] tempColors = getResources().getIntArray(R.array.temp_color_vals);
+            temp_color.setBackgroundColor(tempColors[temperature.getSelectedItemPosition()]);
+        }
+
     }
     
     @Override
     public void onItemSelected (AdapterView<?> parent, View view, int pos, long id) {
     	CalculateResistor();
-    	UpdateColors();
+        UpdateColors();
     }
     
     @Override
